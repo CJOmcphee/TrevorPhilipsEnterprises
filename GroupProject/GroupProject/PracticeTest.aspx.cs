0aca820c6de7979ba1897dbc54759ff201f3be8f
TJ Thomas <tj_thomas_2424@hotmail.ca>
2018-06-12 15:42:22 -0500

    Change

--- GroupProject/GroupProject/PracticeTest.aspx.cs
+++ GroupProject/GroupProject/PracticeTest.aspx.cs
@@ -17,7 +17,7 @@ namespace GroupProject
         public Decimal score;
         public Decimal Total;
         
-        Dictionary<RadioButtonList, Label> radioButtonLists = new Dictionary<RadioButtonList, Label>();
+        Dictionary<RadioButtonList,Tuple<Label, Label>> radioButtonLists = new Dictionary<RadioButtonList, Tuple<Label,Label>>();
         protected void Page_Load(object sender, EventArgs e)
         {
             switch(ddlTestChoice.SelectedItem.Text)
@@ -41,6 +41,7 @@ namespace GroupProject
                 HtmlTableRow trow1 = new HtmlTableRow();
                 HtmlTableCell tCell = new HtmlTableCell();
                 HtmlTableCell tCell1 = new HtmlTableCell();
+                HtmlTableCell tCell2 = new HtmlTableCell();
                 DataSet dsQuestion = Crud.ReadTable("spQuestions", Row[0].ToString());
                 DataSet dsAnswers = Crud.ReadTable("spWrongAnswer", Row[0].ToString());
 
@@ -51,7 +52,6 @@ namespace GroupProject
                 table1.Controls.Add(tRow);
 
                 RadioButtonList myrb = new RadioButtonList();
-                radioButtonLists.Add(myrb,myLabel);
                 myrb.DataSource = dsAnswers.Tables[0];
                 myrb.DataValueField = "wrongAnswers";
                 myrb.DataTextField = "wrongAnswers";
@@ -59,22 +59,30 @@ namespace GroupProject
                 tCell1.Controls.Add(myrb);
                 trow1.Controls.Add(tCell1);
                 table1.Controls.Add(trow1);
+
+                Label answerLabel = new Label();
+                tCell.Controls.Add(answerLabel);
+                tRow.Controls.Add(tCell2);
+                table1.Controls.Add(tRow);
+
+                radioButtonLists.Add(myrb, new Tuple<Label, Label>(myLabel,answerLabel));
             }
         }
         protected void btnSubmit_Click(object sender, EventArgs e)
         {
-            foreach (KeyValuePair<RadioButtonList, Label> radioButton in radioButtonLists)
+            foreach (KeyValuePair<RadioButtonList, Tuple<Label, Label>> radioButton in radioButtonLists)
             {
-                DataSet dsQuestion = Crud.ReadTable("spQuestions", radioButton.Value.Text);
+                DataSet dsQuestion = Crud.ReadTable("spQuestions", radioButton.Value.Item1.Text);
                 if (radioButton.Key.SelectedValue.ToString() == dsQuestion.Tables[0].Rows[0]["answers"].ToString())
                 {
-                    radioButton.Value.Text = radioButton.Value.Text + " " + "Right";
+                    radioButton.Value.Item2.Text = " Right";
+                    radioButton.Value.Item2.ForeColor = System.Drawing.Color.Green;
                     score++;
                 }
                 else
                 {
-                    radioButton.Value.Text = radioButton.Value.Text + " " + "Wrong";
-                    
+                    radioButton.Value.Item2.Text = " Wrong";
+                    radioButton.Value.Item2.ForeColor = System.Drawing.Color.Red;
                 }
             }
             Total = (score / radioButtonLists.Count) * 100;
