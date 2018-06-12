93f7656a8000113e8115a72261684b4f24d72a6a
anton.moyseyenko <a.moyseyenko@gmail.com>
2018-06-11 20:32:13 -0500

    admin stuff

--- GroupProject/GroupProject/admin/index_admin.aspx.designer.cs
+++ GroupProject/GroupProject/admin/index_admin.aspx.designer.cs
@@ -10,24 +10,15 @@
 namespace GroupProject.admin {
     
     
-    public partial class admin {
+    public partial class index {
         
         /// <summary>
-        /// head control.
+        /// form1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
-        
-        /// <summary>
-        /// ContentPlaceHolder1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
     }
 }
