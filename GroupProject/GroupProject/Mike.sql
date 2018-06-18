109eff25a8dc48531d9d8fe75b8e9d2801ee2899
Michael Marini <michaelmarini94@gmail.com>
2018-06-18 12:21:19 -0500

    .....

---
+++ GroupProject/GroupProject/Mike.sql
@@ -0,0 +1,12 @@
+﻿USE dbTestEnviroment;
+GO
+
+--CREATE table tbModule2(
+--ID int IDENTITY (0,1) PRIMARY KEY,
+--Words varchar(200)
+--)
+
+--INSERT INTO dbo.tbModule2 (Words) VALUES ('This is a word'),('Word-two')
+
+--SELECT * FROM dbo.tbModule2;
+
