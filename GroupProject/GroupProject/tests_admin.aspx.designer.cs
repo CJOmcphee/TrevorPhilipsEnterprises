d59c1f84be4a865e4fefa9928c7367266c198475
TJ Thomas <tj_thomas_2424@hotmail.ca>
2018-06-28 11:05:39 -0500

    asdasd

--- GroupProject/GroupProject/tests_admin.aspx.designer.cs
+++ GroupProject/GroupProject/tests_admin.aspx.designer.cs
@@ -56,5 +56,23 @@ namespace GroupProject.admin {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView gvQuestions;
+        
+        /// <summary>
+        /// pnlEditQuestion control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel pnlEditQuestion;
+        
+        /// <summary>
+        /// gvWrongAnswers control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gvWrongAnswers;
     }
 }
