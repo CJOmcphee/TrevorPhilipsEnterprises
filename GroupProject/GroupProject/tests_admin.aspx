1ea1b78a0e4ccd609f1f5526e43aed2e2545cf7a
TJ Thomas <tj_thomas_2424@hotmail.ca>
2018-06-28 13:06:08 -0500

    asdasda

--- GroupProject/GroupProject/tests_admin.aspx
+++ GroupProject/GroupProject/tests_admin.aspx
@@ -13,7 +13,7 @@
                 <asp:GridView ID="gvTests" PageSize="5" AutoGenerateColumns="false" AllowPaging="true" DataKeyNames="testID" runat="server" OnRowCommand="gvTests_RowCommand" OnPageIndexChanging="gvTests_PageIndexChanging" >
                       <Columns>
                         <asp:ButtonField HeaderText="Delete" ButtonType="Button" CommandName="Del" Text="Delete" />
-                        <asp:ButtonField HeaderText="Update" ButtonType="Button" CommandName="Edi" Text="Edit" />
+                        <asp:ButtonField HeaderText="Edit" ButtonType="Button" CommandName="Edi" Text="Edit" />
                         <asp:BoundField HeaderText="Module" DataField="testID" />
                       </Columns>
                 </asp:GridView>
@@ -25,25 +25,40 @@
                  <asp:GridView ID="gvQuestions" PageSize="10" AutoGenerateColumns="false" AllowPaging="true" DataKeyNames="question" runat="server" OnRowCommand="gvQuestions_RowCommand" OnPageIndexChanging="gvQuestions_PageIndexChanging" >
                       <Columns>
                         <asp:ButtonField HeaderText="Delete" ButtonType="Button" CommandName="Del" Text="Delete" />
-                        <asp:ButtonField HeaderText="Update" ButtonType="Button" CommandName="Edi" Text="Edit" />
+                        <asp:ButtonField HeaderText="Edit" ButtonType="Button" CommandName="Edi" Text="Edit" />
                         <asp:BoundField HeaderText="Question" DataField="question" />
                         <asp:BoundField HeaderText="Answer" DataField="answers" />
                         <asp:BoundField HeaderText="# Wrong Answers" DataField="wrongAnswers" />
                       </Columns>
                 </asp:GridView>
+                Question
+                <asp:TextBox ID="tbNewQuestion" runat="server"></asp:TextBox>
+                Answer
+                <asp:TextBox ID="tbNewAnswer" runat="server"></asp:TextBox>
+                <asp:Button ID="btnAddQuestion" runat="server" Text="Add Question" OnClick="btnAddQuestion_Click" />
             </asp:Panel>
 
 
         <asp:Panel ID="pnlEditQuestion" Visible="false" runat="server">
-                 <asp:GridView ID="gvWrongAnswers" PageSize="10" AutoGenerateColumns="false" AllowPaging="true" DataKeyNames="wrongAnswers" runat="server" >
+            <asp:Button ID="btnAdd" runat="server" Text="Add wrong answer" OnClick="btnAdd_Click" />
+                 <asp:GridView ID="gvWrongAnswers" PageSize="10" AutoGenerateColumns="false" AllowPaging="true" DataKeyNames="wrongAnswers" runat="server" OnPageIndexChanging="gvWrongAnswers_PageIndexChanging" OnRowCommand="gvWrongAnswers_RowCommand" >
                       <Columns>
                         <asp:ButtonField HeaderText="Delete" ButtonType="Button" CommandName="Del" Text="Delete" />
-                        <asp:ButtonField HeaderText="Update" ButtonType="Button" CommandName="Edi" Text="Edit" />
-                        <asp:BoundField HeaderText="Question" DataField="question" />
-                        <asp:BoundField HeaderText="Answer" DataField="answers" />
-                        <asp:BoundField HeaderText="# Wrong Answers" DataField="wrongAnswers" />
+                        <asp:BoundField HeaderText="Wrong Answers" DataField="wrongAnswers" />
                       </Columns>
                 </asp:GridView>
+            <asp:Panel ID="pnlQuestionDetails" runat="server">
+                Question
+                <asp:TextBox ID="tbQuestionDetail" runat="server"></asp:TextBox>
+                Answer
+                <asp:TextBox ID="tbAnswerDetail" runat="server"></asp:TextBox>
+                <asp:Button ID="btnSave" runat="server" Text="Change Question" OnClick="btnSave_Click" />
+            </asp:Panel>
+            <asp:Panel ID="pnlAddWrongAnswer" Visible="false" runat="server">
+                Wrong Answer
+                <asp:TextBox ID="tbWrongAnswer" runat="server"></asp:TextBox>
+                <asp:Button ID="btnAddWrongAnswer" runat="server" Text="Add Wrong Answer" OnClick="btnAddWrongAnswer_Click" />
+            </asp:Panel>
             </asp:Panel>
    </div>
  
--- GroupProject/GroupProject/tests_admin.aspx.cs
+++ GroupProject/GroupProject/tests_admin.aspx.cs
@@ -11,35 +11,74 @@ namespace GroupProject.admin
 {
     public partial class tests_admin : System.Web.UI.Page
     {
+        public string Test;
+        public string Question;
         protected void Page_Load(object sender, EventArgs e)
         {
-            LoadView();
+            if (Session["Test"] != null)
+            {
+                Test = (string)Session["Test"];
+            }
+            if(Session["Question"] != null)
+            {
+                Question = (string)Session["Question"];
+            }
+                   
+            if (!IsPostBack)
+            {
+                LoadTest(Crud.ReadTable("spTest"));
+            }
         }
 
         protected void btnAddTest_Click(object sender, EventArgs e)
         {
+            
+        }
+        protected void btnAddQuestion_Click(object sender, EventArgs e)
+        {
 
         }
-        public void LoadView()
+        protected void btnAddWrongAnswer_Click(object sender, EventArgs e)
         {
-            pnlTestsList.Visible = true;
-            gvTests.DataSource = Crud.ReadTable("spTest");
+
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+        public void LoadTest(DataSet ds)
+        {
+            gvTests.DataSource = ds;
             gvTests.DataBind();
+            pnlTestsList.Visible = true;
+        }
+        public void LoadQuestion(DataSet ds)
+        {
+            gvQuestions.DataSource = ds;
+            gvQuestions.DataBind();
+            pnlQuestion.Visible = true;
+        }
+        public void LoadWrongAnswer(DataSet ds)
+        {
+            gvWrongAnswers.DataSource = ds.Tables[1];
+            gvWrongAnswers.DataBind();
+            pnlEditQuestion.Visible = true;
         }
         protected void gvTests_RowCommand(object sender, GridViewCommandEventArgs e)
         {
             gvTests.SelectedIndex = Convert.ToInt32(e.CommandArgument);
+            Session["Test"] = gvTests.SelectedDataKey["testID"].ToString();
+            Test = (string)Session["Test"];
             switch (e.CommandName)
             {
                 case "Edi":
                     pnlTestsList.Visible = false;
-                    gvQuestions.DataSource = Crud.GetTestQuestions(gvTests.SelectedDataKey["testID"].ToString());
-                    gvQuestions.DataBind();
-                    pnlQuestion.Visible = true;
+                    LoadQuestion(Crud.GetTestQuestions(Test));
                     break;
                 case "Del":
-                    Crud.DeleteData("spTest", gvTests.SelectedDataKey["testID"].ToString());
-                    LoadView();
+                    Crud.DeleteData("spTest", Test);
+                    LoadTest(Crud.ReadTable("spTest"));
                     break;
             }
         }
@@ -47,27 +86,44 @@ namespace GroupProject.admin
         protected void gvQuestions_RowCommand(object sender, GridViewCommandEventArgs e)
         {
             gvQuestions.SelectedIndex = Convert.ToInt32(e.CommandArgument);
+            Session["Question"] = gvQuestions.SelectedDataKey["question"].ToString();
+            Question = (string)Session["Question"];
             switch (e.CommandName)
             {
                 case "Edi":
-
+                    pnlQuestion.Visible = false;
+                    LoadWrongAnswer(Crud.ReadTable("spWrongAnswer",Question));
                     break;
                 case "Del":
-
+                    Crud.DeleteData("spQuestion", Question);
+                    LoadQuestion(Crud.GetTestQuestions(Test));
                     break;
             }
         }
+        protected void gvWrongAnswers_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            gvWrongAnswers.SelectedIndex = Convert.ToInt32(e.CommandArgument);
+            Crud.DeleteData("spWrongAnser", gvWrongAnswers.SelectedDataKey["wrongAnswer"].ToString());
 
+        }
         protected void gvTests_PageIndexChanging(object sender, GridViewPageEventArgs e)
         {
             gvTests.PageIndex = e.NewPageIndex;
-            LoadView();
+            LoadTest(Crud.ReadTable("spTest"));
         }
 
         protected void gvQuestions_PageIndexChanging(object sender, GridViewPageEventArgs e)
         {
             gvQuestions.PageIndex = e.NewPageIndex;
-            LoadView();
+            LoadQuestion(Crud.GetTestQuestions(Test));
         }
+
+        protected void gvWrongAnswers_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            gvWrongAnswers.PageIndex = e.NewPageIndex;
+            LoadWrongAnswer(Crud.ReadTable("spWrongAnswer", Question));
+        }
+
+
     }
 }
