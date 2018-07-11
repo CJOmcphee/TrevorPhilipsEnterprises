e53c21719c7831a8c84b7ba5a1e24c4b63404509
chris mcphee <chris_kim@hotmail.com>
2018-07-11 11:42:57 -0500

    asdfsadfdsa

--- GroupProject/GroupProject/LOTR/LOTRSecurity.cs
+++ GroupProject/GroupProject/LOTR/LOTRSecurity.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.SessionState;
 
 namespace GroupProject
 {
     public class LOTRSecurity
     {
+        SqlConnection conn = new SqlConnection("Data Source=localhost;Initial Catalog=dbLOTR;Integrated Security=SSPI;");
+        public string Access { get; set; }
+        public string FullName { get; set; }
+        public string clientID { get; set; }
+        public string userName { get; set; }
+        public string password { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
     }
 }
