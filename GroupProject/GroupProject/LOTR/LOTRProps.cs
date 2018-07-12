dceaff99eed0de1055c46537165fb5dae80e9328
chris mcphee <chris_kim@hotmail.com>
2018-07-12 13:58:13 -0500

    LOTR

---
+++ GroupProject/GroupProject/LOTR/LOTRProps.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GroupProject
+{
+    public class LOTRProps
+    {
+        List<LOTRProp> _LOTRProps;
+        public LOTRProps()
+        {
+            if (HttpContext.Current.Session["Product"] != null)
+            {
+                _LOTRProps = (List<LOTRProp>)HttpContext.Current.Session["LOTRProp"];
+            }
+            else
+            {
+                _LOTRProps = new List<LOTRProp>();
+            }
+        }
+        public void Save()
+        {
+            HttpContext.Current.Session["LOTRProp"] = _LOTRProps;
+
+        }
+    }
+}
