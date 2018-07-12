3e7b29d6ec7f39a2a27af0a42cf39509e48ea31a
chris mcphee <chris_kim@hotmail.com>
2018-07-12 14:29:26 -0500

    LOTR

--- GroupProject/GroupProject/LOTR/LOTRProps.cs
+++ GroupProject/GroupProject/LOTR/LOTRProps.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace GroupProject
 {
@@ -23,6 +25,51 @@ namespace GroupProject
         {
             HttpContext.Current.Session["LOTRProp"] = _LOTRProps;
 
+        }
+        public void Add(LOTRArmour armour)
+        {
+            _LOTRProps.Add(armour);
+            Save();
+        }
+        public void Add(LOTRSword sword)
+        {
+            _LOTRProps.Add(sword);
+            Save();
+        }
+        public void RemoveProd(int ID)
+        {
+
+            LOTRProp productToDelete = null;
+            foreach (LOTRProp prod in _LOTRProps)
+            {
+                if (prod.id == ID)
+                {
+                    productToDelete = prod;
+                    _LOTRProps.Remove(productToDelete);
+                    break;
+                }
+            }
+        }
+        public DataTable Get()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("id");
+            dt.Columns.Add("product");
+            dt.Columns.Add("type");
+            dt.Columns.Add("price");
+
+            foreach (LOTRProp p in _LOTRProps)
+            {
+                DataRow r = dt.NewRow();
+                r["id"] = p.id;
+                r["product"] = p.name;
+                r["type"] = p.type;
+                r["price"] = p.price;
+
+                dt.Rows.Add(r);
+            }
+
+            return dt;
         }
     }
 }
