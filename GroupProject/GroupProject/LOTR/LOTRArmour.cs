bafe58b154c767d5031706cabaa9049ee5391d4a
chris mcphee <chris_kim@hotmail.com>
2018-07-12 14:23:24 -0500

    LOTR

---
+++ GroupProject/GroupProject/LOTR/LOTRArmour.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GroupProject
+{
+    public class LOTRArmour:LOTRProp
+    {
+        public LOTRArmour(int _id, string _name, string _type, double _price)
+        {
+            base.name = _name;
+            base.type = _type;
+            base.price = _price;
+            base.id = _id;
+        }
+    }
+}
