e27c5f443f35c66fae94cdb7c80e350d18149d27
TJ Thomas <tj_thomas_2424@hotmail.ca>
2018-07-16 18:47:18 -0500

    sdfsdfsdf

--- GroupProject/GroupProject/SlideshowTemplate.aspx.designer.cs
+++ GroupProject/GroupProject/SlideshowTemplate.aspx.designer.cs
@@ -47,5 +47,14 @@ namespace GroupProject {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnNext;
+        
+        /// <summary>
+        /// btnGoToExample control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnGoToExample;
     }
 }
