499ef033331ae28fe056c68a35dc60533a5a861a
anton.moyseyenko <a.moyseyenko@gmail.com>
2018-07-26 20:43:51 -0500

    module 7

--- GroupProject/GroupProject/Anton.sql
+++ GroupProject/GroupProject/Anton.sql
@@ -1,7 +1,7 @@
 ﻿use dbTestEnviroment;
 go
 
-EXEC dbo.spSlides @slideID = '7-3-1',   -- varchar(50)
-                  @lessonid = '7-3',  -- varchar(50)
+EXEC dbo.spSlides @slideID = '7-5-1',   -- varchar(50)
+                  @lessonid = '7-5',  -- varchar(50)
                   @slideinfo = '', -- varchar(1000)
                   @crud = 'c'       -- varchar(1)
