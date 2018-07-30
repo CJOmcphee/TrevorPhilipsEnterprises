aac5cf985155752bdb402fc9b05b893539a0fafa
Michael Marini <michaelmarini94@gmail.com>
2018-07-30 11:52:55 -0500

    DataEntry.sql commit module 7 Lesson 4

--- GroupProject/GroupProject/DataEntry.sql
+++ GroupProject/GroupProject/DataEntry.sql
@@ -4276,7 +4276,84 @@ EXEC spSlides	@slideID = '6-4-11',
 				@crud = 'c'
 
 --Module 7 Slides
+EXEC dbo.spSlides @slideID = '7-4-1',   -- varchar(50)
+                  @lessonid = '7-4',  -- varchar(50)
+                  @slideinfo = 'Data Structures ^ (Queues, Stacks, LinkLists and Nodes)', -- varchar(2000)
+                  @crud = 'c'       -- varchar(1)
 
+
+EXEC dbo.spSlides @slideID = '7-4-2',   -- varchar(50)
+                  @lessonid = '7-4',  -- varchar(50)
+                  @slideinfo = 'Data Structures ^^ -In the previous Lesson we made our own MyList class ^ which had mehtods used to Add/Remove/Print an array of ^ integers ^^ -In this lesson we will discuess a few other types of common ^ Data Structures such as : ^^ *Queues ^ *Stacks ^ *Nodes and LinkedLists', -- varchar(2000)
+                  @crud = 'c'       -- varchar(1)
+
+EXEC dbo.spSlides @slideID = '7-4-3',   -- varchar(50)
+                  @lessonid = '7-4',  -- varchar(50)
+                  @slideinfo = 'Queues ^^ -A Queue is a type of data structure which has a particular ^ way of handling adding/removing items in a list ^^ -Just like the line up at a fast food restaurant, a Queue is ^ First In First Out (FIFO) ^^ -When you add values to the Queue, they Enwueue() (or ^ Push) behind the values that came before them ^^ -When you remove values from a Queue, they Dequeue() (or ^ Pop) from the list based on who was added first.', -- varchar(2000)
+                  @crud = 'c'       -- varchar(1)
+
+EXEC dbo.spSlides @slideID = '7-4-4',   -- varchar(50)
+                  @lessonid = '7-4',  -- varchar(50)
+                  @slideinfo = 'Stacks ^^ -A Stack is a type of data structure which acts like a stack of ^ plates in terms of handling data ^^ -A Stack is First In Last Out (FILO) ^^ -When you add values to the Stack, they Push() behind the ^ values that came before them ^^ -When you remove values from a Stack, they Pop() from the ^ list based on who was added first!', -- varchar(2000)
+                  @crud = 'c'       -- varchar(1)
+
+EXEC dbo.spSlides @slideID = '7-4-5',   -- varchar(50)
+                  @lessonid = '7-4',  -- varchar(50)
+                  @slideinfo = 'LinkedListNode ^^ -A LinkedListNode is a class which contains only a few ^ properties: ^^ *Value: an object such as an int/string/class ^ *Next: A reference to the Next Node ^ *Previous: A reference to the Previous Node ^^ -We can make a list of values by chaining each Node ^ together ^^ -C# already has a built in data structure which handles ^ adding and removing Nodes in a list...', -- varchar(2000)
+                  @crud = 'c'       -- varchar(1)
+
+EXEC dbo.spSlides @slideID = '7-4-6',   -- varchar(50)
+                  @lessonid = '7-4',  -- varchar(50)
+                  @slideinfo = 'LinkedList ^^ -A LinkedList is a type of data structure which allows you to ^ add/remove easily from either end ^^ -When you add values to a LinkedList, you can choose to ^ add values to the end or the front: ^ *AddLast(), AddFirst() ^^ - When you remove values from a LinkedList, you can again ^ remove from the front or the back: ^ *RemoveLast(), RemoveFirst()', -- varchar(2000)
+                  @crud = 'c'       -- varchar(1)
+
+EXEC dbo.spSlides @slideID = '7-4-7',   -- varchar(50)
+                  @lessonid = '7-4',  -- varchar(50)
+                  @slideinfo = 'Nodes ^^ -We should understand how Nodes are connected on a more ^ fundamental level ^^ -If we were to make our own Node class with just two ^ properties: ^ *Value ^ *NextNode ^^ -How would we connect the nodes without a LinkedList ^ class?', -- varchar(2000)
+                  @crud = 'c'       -- varchar(1)
+
+EXEC dbo.spSlides @slideID = '7-4-8',   -- varchar(50)
+                  @lessonid = '7-4',  -- varchar(50)
+                  @slideinfo = 'Nodes ^^ -As you can see from the last slide, the first node has a ^ reference to the second node ^^ -If we were to make a third node, we would be able to make ^ a reference to it from the second node (and so forth to ^ forever) ^^ -As you can see, as long as we can start at the first node ^ (RootNode), we can loop through the nodes using the "NextNode" property', -- varchar(2000)
+                  @crud = 'c'       -- varchar(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--End of Module 7 slides
 EXEC dbo.spSlides @slideID = '7-1-1',   -- varchar(50)
                   @lessonid = '7-1',  -- varchar(50)
                   @slideinfo = 'Object Oriented Progmanning ^ What is Object Oriented Porgramming (OOP)? ^ OOP is code design philosophy ^ Is evolved naturally out of Porcedural Porgramming^ OOP thinks about everything as having properties or methods ^ Major concepts of OPP ^ Inheritance, Polymorphism and Encapsulation', -- varchar(1000)
