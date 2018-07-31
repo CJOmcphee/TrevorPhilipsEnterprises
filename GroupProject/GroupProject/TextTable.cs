20b7b3d1c0b61a9bc62d2012a03f6eb73d9b4954
Darryl Wilson <d_wilson93@hotmail.com>
2018-07-31 15:52:51 -0500

    texttable

--- GroupProject/GroupProject/TextTable.cs
+++ GroupProject/GroupProject/TextTable.cs
@@ -12,42 +12,58 @@ namespace GroupProject
         static int cellID = 0;
         private void takeTable(int tblNum,int rowNum, int cellNum)
         {
-
+            //This finds and creates the tables, rows, and cells when 
+            //the C# will find and/or give a number for each table, row and cell it finds or wants to create.
+            //Once this is done, it will then plug every number in accordingly to create
+            //and store these tables for later use.
+            newTable(tblNum); //Creates the number of TABLES found or entered in.
+            newRow(tblNum, rowNum); //Creates the number of ROWS found or entered in.
+            newCell(tblNum, rowNum, cellNum);//Creates the number of CELLS found or entered in.
         }
-        public string newTable(int tblNum,int rowNum, int cellNum)
+
+
+        public string newTable(int tblNum)
         {
-            string Table ="";
+            string Table =""; 
+            // Creates an HTML table in text for as many tables that the app counts.
+            //but we need to build the return variable first in each call of each newTable.
             for (int x = 0; x < tblNum; x++)
             {
-                tblID++;
+                //The FOR loop goes through every instance there is a table,
+                //so that if there is more than one table, it will create more than one table.
+                tblID = x;
                 string Start = "<table>";
-                string Mid = newRow(rowNum, cellNum);
+                string Mid = "";
                 string End = "</table>";
                 Table = Table + Start + Mid + End;
+                //The STRING "Table" is created so that we have a created return value that we can call later.
             }
             return Table;
         }
 
-        private string newRow(int rowNum, int cellNum)
+        private string newRow(int tblNum, int rowNum)
         {
             string Row = "";
             for (int x = 0; x < rowNum; x++)
             {
-                rowID++;
+                tblID = tblNum;
+                rowID = x;
                 string Start = "<tr>";
-                string Mid = newCell(cellNum);
+                string Mid = "";
                 string End = "</tr>";
                 Row = Row + Start + Mid + End;
             }
             return Row;
         }
 
-        private string newCell(int cellNum)
+        private string newCell(int tblNum, int rowNum, int cellNum)
         {
             string Cell = "";
             for (int x = 0; x < cellNum; x++)
             {
-                cellID++;
+                tblID = tblNum;
+                rowID = rowNum;
+                cellID = x;
                 string Start = "<td>";
                 string Mid = "";
                 string End = "</td>";
