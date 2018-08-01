4b0a949de1a5f0fa1c1b8f06d43ddf7331fe3f13
anton.moyseyenko <a.moyseyenko@gmail.com>
2018-07-31 20:21:51 -0500

    scripts updated

--- GroupProject/GroupProject/js/init.js
+++ GroupProject/GroupProject/js/init.js
@@ -54,13 +54,3 @@ skel.init({
 	
 	
     });
-
-$(document).ready(function () {
-    var url = window.location;
-    $('#nav').find('.active').removeClass('active');
-    $('#nav li a').each(function () {
-        if (this.href == url) {
-            $(this).parent().addClass('active');
-        }
-    });
-});
